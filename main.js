e3d3924dbecd8d78b5eea58f1f9f2bf57e9d6a2f
surya teja <TEJA@MICROSOFT.com>
2019-05-28 11:29:31 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    const connect4 = new Connect4('#connect4')
+  
+    connect4.onPlayerMove = function() {
+      $('#player').text(connect4.player);
+    }
+    
+    $('#restart').click(function() {
+      connect4.restart();
+    })
+  });
+
+
+  
+  
+  
